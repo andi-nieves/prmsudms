9c7b835602aca28d5269b78875e173c74bb06ceb
Andy Nieves <anieves@unionbankph.com>
2022-12-08 18:24:01 +0800

    Move html head to separate callable component

---
+++ inc/html-head.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $title ?></title>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $SITE_NAME ?>/css/style.css">
+</head>
