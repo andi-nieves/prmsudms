5036cd1f2a9043ebbb94f92c0724bedcdc646cf1
Andy Nieves <anieves@unionbankph.com>
2022-12-09 16:38:09 +0800

    Refactor entry form

--- admin/students/entry.php
+++ admin/students/entry.php
@@ -1,35 +1,53 @@
 <?php 
-require_once('../../config.php');
-$title = "Student Entry";
+    require_once('../../config.php');
+    $title = "Student Entry";
+    $page = $_GET['page'] ?? null;
+    $student_data = null;
+    $student_id = $_GET['id'] ?? null;
+    $age = null;
+    if (!is_null($student_id)) {
+        require_once '../../classes/db_helper.php';
+        $db = new db();
+        $student_data = $db->query("SELECT * FROM `student_list` WHERE id=:id", array(":id"=>$student_id))[0] ?? null;
+        $age = floor((time() - strtotime($student_data->birthdate)) / 31556926);
+    }
 ?>
 <!DOCTYPE html>
-    <html>
-    <?php include $ROOT_DIR.'/inc/html-head.php' ?>
-    <body>
-        <!--
-            <ul>
-                <li class="user-icon">
-                <img src="img/user.jpg" class="uicon">
-                </li>
-            </ul>
-            <!-->
-        <div class="wrapper">
-            <div class="section">
-                <?php include $ROOT_DIR.'/inc/header.php'; ?>
-            </div>
-            <?php include $ROOT_DIR.'/inc/sidebar.php'; ?>
-            <div class="content-wrapper" style="min-height:628.038px">
-                <section class="content">
-                    <div class="container">
-                        <form id="student-entry-form">
-                            <?php include 'form.php' ?>
-                        </form>
-                        
-                    </div>
-                </section>
-            </div>
-            <?php include $ROOT_DIR.'/inc/footer.php'; ?>
-        </div>
+<html>
+<?php include $ROOT_DIR.'/inc/html-head.php' ?>
 
-    </body>
-    </html>
+<body>
+    <div class="wrapper">
+        <div class="section">
+            <?php include $ROOT_DIR.'/inc/header.php'; ?>
+        </div>
+        <?php include $ROOT_DIR.'/inc/sidebar.php'; ?>
+        <div class="content-wrapper" style="min-height:628.038px">
+            <section class="content">
+                <div class="container">
+
+                    <?php 
+                        if (!is_null($student_data)) {
+                            switch($page) {
+                                case 'new': case 'edit':
+                                    include 'form.php';
+                                    break;
+                                default:
+                                    include 'view.php';
+                                    break;
+                            }
+                        } else {
+                            include '../../components/no-record.html';
+                        }
+                        ?>
+                    </form>
+
+                </div>
+            </section>
+        </div>
+        <?php include $ROOT_DIR.'/inc/footer.php'; ?>
+    </div>
+
+</body>
+
+</html>
