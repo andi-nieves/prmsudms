054a46bbdaac42a38ef4f446923dc671229d5c43
Andy Nieves <anieves@unionbankph.com>
2022-12-09 18:35:35 +0800

    Fix style import isuee

--- inc/html-head.php
+++ inc/html-head.php
@@ -5,5 +5,5 @@
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $SITE_NAME ?>/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
 </head>
