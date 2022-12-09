0c28018f3835f787f805115e031effc71ebf4620
Andy Nieves <anieves@unionbankph.com>
2022-12-09 16:40:35 +0800

    Student CRUD api

--- api/student-entry.php
+++ api/student-entry.php
@@ -1,11 +1,18 @@
 <?php
     require_once '../classes/db_helper.php';
     $table_name = 'student_list';
+    if (isset($_GET['type']) && $_GET['type'] == 'delete') {
+        $db->_cmd("DELETE FROM $table_name WHERE id=:id", array(":id" => $_POST['id']));
+        echo json_encode(array('success'=>true));
+        return;
+    }
     if (is_null($_POST['id'])) {
-        $db->generate_insert_sql($table_name, $_POST);
+        $cmd = $db->generate_insert_sql($table_name, $_POST);
+        echo json_encode(array('success'=>true, 'cmd'=>$cmd));
     } else {
         $id = $_POST['id'];
         unset($_POST['id']);
         $db->generate_update_sql($table_name, $id, $_POST);
+        echo json_encode(array('success'=>true, 'id'=>$id));
     }
 ?>
