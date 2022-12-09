e5fb6eebc9a74e129bdfeb8ee22fce1da6ca66fb
Andy Nieves <anieves@unionbankph.com>
2022-12-09 16:41:29 +0800

    Add no record display

--- inc/footer.php
+++ inc/footer.php
@@ -5,3 +5,16 @@
     </strong>
     <p> All rights reserved.</p>
 </footer>
+
+<div class="modal" style="display: none">
+    <div class="modal-content">
+        <div class="modal-header">
+            New Payments
+        </div>
+        <div class="modal-body">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae quod optio eligendi natus deserunt saepe rem
+            quis qui nesciunt! Vero veniam, accusantium ipsum asperiores rem doloremque repudiandae voluptate illo
+            placeat.
+        </div>
+    </div>
+</div>
