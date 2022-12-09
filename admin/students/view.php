2ad6a41f5d3d6305a28e0d7e10db7a128ca5da6f
Andy Nieves <anieves@unionbankph.com>
2022-12-09 19:20:46 +0800

    Refactor view buttons

--- admin/students/view.php
+++ admin/students/view.php
@@ -143,9 +143,51 @@
             </div>
             <input type="hidden" name="id" value="4" />
             <div class="justify-content m-t">
+                <button id="btn-payment-history" class="btn btn-default m-r" data-id="<?php echo $student_id ?>">Payment
+                    History</button>
+                <button id="btn-add-payment" class="btn btn-default m-r" data-id="<?php echo $student_id ?>">Add
+                    Payment</button>
                 <a class="btn btn-default m-r" href="/admin/students/entry.php?id=<?php echo $student_id ?>&page=edit">Edit</a>
-                <button id="delete" data-id="<?php echo $student_id ?>" class="btn btn-danger m-r">Delete</button>
-                <a class="btn btn-flat" href="<?php echo $SITE_NAME ?>/admin/student.php">Cancel</a>
+                <button id="delete" data-id="<?php echo $student_id ?>" data-code="<?php echo $student_data->code ?>"
+                    data-name="<?php echo "$student_data->firstname $student_data->lastname" ?>"
+                    class="btn btn-danger m-r">Delete</button>
+                <a class="btn btn-flat" href="<?php echo $SITE_NAME ?>/admin/student.php">Back</a>
             </div>
         </div>
     </div>
+
+    <script>
+    $("#btn-add-payment").on('click', function() {
+        modal({
+            title: 'New Payment',
+            body: `<div>
+                    <div class="input-wrapper">
+                                <div><span>Month of</span></div>
+                                <input name="" type="date"/>
+                            </div>
+                            <div class="input-wrapper">
+                                <div><span>Amount</span></div>
+                                <input name="" type="number"/>
+                            </div>
+            </div>
+            `,
+            buttons: [{
+                label: 'Save',
+                class: 'btn-default',
+                action: function() {
+
+                }
+            }]
+        })
+    })
+
+    $("#btn-payment-history").on('click', function() {
+        modal({
+            title: 'New Payment',
+            body: `<div>
+                    // TODO
+            </div>
+            `,
+        })
+    })
+    </script>
