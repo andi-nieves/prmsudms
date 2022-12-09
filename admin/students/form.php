1c289f685e067e78f01d551f76aa9eb24d18508f
Andy Nieves <anieves@unionbankph.com>
2022-12-09 18:33:33 +0800

    Fix error on student data nil

--- admin/students/form.php
+++ admin/students/form.php
@@ -36,7 +36,7 @@ $courses = array(
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Student Number</span></div>
-                                <input name="code" value="<?php echo $student_data->code ?>" />
+                                <input name="code" value="<?php echo $student_data->code ?? "" ?>" />
                             </div>
                         </div>
                         <div class="col">
@@ -45,7 +45,7 @@ $courses = array(
                                 <select name="course" value="<?php echo $student_data->course ?>">
                                     <?php
                                         foreach($courses as $course) {
-                                            $selected = $student_data->course === $course ? 'selected' : '';
+                                            $selected = $student_data->course ?? "" === $course ? 'selected' : '';
                                             echo "<option $selected>$course</option>";
                                         }
                                     ?>
@@ -57,7 +57,7 @@ $courses = array(
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Department</span></div>
-                                <input name="department" value="<?php echo $student_data->department ?>" />
+                                <input name="department" value="<?php echo $student_data->department ?? "" ?>" />
                             </div>
                         </div>
                     </div>
@@ -68,19 +68,19 @@ $courses = array(
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>First Name</span></div>
-                                <input name="firstname" value="<?php echo $student_data->firstname ?>" />
+                                <input name="firstname" value="<?php echo $student_data->firstname ?? "" ?>" />
                             </div>
                         </div>
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Middle Name</span></div>
-                                <input name="middlename" value="<?php echo $student_data->middlename ?>" />
+                                <input name="middlename" value="<?php echo $student_data->middlename ?? "" ?>" />
                             </div>
                         </div>
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Last Name</span></div>
-                                <input name="lastname" value="<?php echo $student_data->lastname ?>" />
+                                <input name="lastname" value="<?php echo $student_data->lastname ?? "" ?>" />
                             </div>
                         </div>
                     </div>
@@ -89,22 +89,22 @@ $courses = array(
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Birthday</span></div>
-                                <input name="birthdate" value="<?php echo $student_data->birthdate ?>" type="date" />
+                                <input name="birthdate" value="<?php echo $student_data->birthdate ?? "" ?>" type="date" />
                             </div>
                         </div>
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Age</span></div>
-                                <div class="static"><?php echo $age ?></div>
+                                <div class="static"><?php echo $age ?? "" ?></div>
                             </div>
                         </div>
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Gender</span></div>
                                 <select name="gender">
-                                    <option <?php echo $student_data->gender == "Male" ? "selected" : "" ?>>Male
+                                    <option <?php echo $student_data->gender ?? "" == "Male" ? "selected" : "" ?>>Male
                                     </option>
-                                    <option <?php echo $student_data->gender == "Female" ? "selected" : "" ?>>Female
+                                    <option <?php echo $student_data->gender ?? "" == "Female" ? "selected" : "" ?>>Female
                                     </option>
                                 </select>
                             </div>
@@ -115,19 +115,19 @@ $courses = array(
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Religion</span></div>
-                                <input name="religion" value="<?php echo $student_data->religion ?>" />
+                                <input name="religion" value="<?php echo $student_data->religion ?? "" ?>" />
                             </div>
                         </div>
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Contact Number</span></div>
-                                <input name="contact" value="<?php echo $student_data->contact ?>" />
+                                <input name="contact" value="<?php echo $student_data->contact ?? "" ?>" />
                             </div>
                         </div>
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Email</span></div>
-                                <input name="email" value="<?php echo $student_data->email ?>" />
+                                <input name="email" value="<?php echo $student_data->email ?? "" ?>" />
                             </div>
                         </div>
                     </div>
@@ -136,7 +136,7 @@ $courses = array(
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Address</span></div>
-                                <textarea name="address"><?php echo $student_data->address ?></textarea>
+                                <textarea name="address"><?php echo $student_data->address ?? "" ?></textarea>
                             </div>
                         </div>
                     </div>
@@ -148,21 +148,21 @@ $courses = array(
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Name</span></div>
-                                <input name="emergency_name" value="<?php echo $student_data->emergency_name ?>" />
+                                <input name="emergency_name" value="<?php echo $student_data->emergency_name ?? "" ?>" />
                             </div>
                         </div>
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Contact Number</span></div>
                                 <input name="emergency_contact"
-                                    value="<?php echo $student_data->emergency_contact ?>" />
+                                    value="<?php echo $student_data->emergency_contact ?? "" ?>" />
                             </div>
                         </div>
                         <div class="col">
                             <div class="input-wrapper">
                                 <div><span>Relation</span></div>
                                 <input name="emergency_relation"
-                                    value="<?php echo $student_data->emergency_relation ?>" />
+                                    value="<?php echo $student_data->emergency_relation ?? "" ?>" />
                             </div>
                         </div>
                     </div>
@@ -171,7 +171,7 @@ $courses = array(
                             <div class="input-wrapper">
                                 <div><span>Address</span></div>
                                 <textarea
-                                    name="emergency_address"><?php echo $student_data->emergency_address ?></textarea>
+                                    name="emergency_address"><?php echo $student_data->emergency_address ?? "" ?></textarea>
                             </div>
                         </div>
                     </div>
@@ -179,14 +179,14 @@ $courses = array(
                 <div class="input-wrapper">
                     <div><span>Status</span></div>
                     <select name="status">
-                        <option value="1" <?php echo $student_data->status == "1" ? "selected" : "" ?>>Active</option>
-                        <option value="0" <?php echo $student_data->status == "0" ? "selected" : ""  ?>>Inactive
+                        <option value="1" <?php echo $student_data->status ?? "" == "1" ? "selected" : "" ?>>Active</option>
+                        <option value="0" <?php echo $student_data->status ?? "" == "0" ? "selected" : ""  ?>>Inactive
                         </option>
                     </select>
                 </div>
                 <input type="hidden" name="id" value="<?php echo $student_id ?>" />
                 <div class="justify-content m-t">
-                    <button class="btn btn-default" type="submit">Save</button>
+                    <button class="btn btn-default m-r" type="submit">Save</button>
                     <a class="btn btn-flat" href="<?php echo $SITE_NAME ?>/admin/student.php">Cancel</a>
                 </div>
             </div>
