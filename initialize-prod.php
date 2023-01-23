596aff731c0ccb749b50af0498af32e6ceaf1f47
andi-nieves <andinieves151720@gmail.com>
2023-01-23 20:32:42 +0800

    sandbox

---
+++ initialize-prod.php
@@ -0,0 +1,10 @@
+<?php
+if(!defined('base_url')) define('base_url','http://localhost/');
+if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+if(!defined('servername')) define('servername',"localhost");
+if(!defined('username')) define('username',"root");
+if(!defined('password')) define('password',"");
+if(!defined('db_name')) define('db_name',"PRMSUdms_db");
+if(!defined('SECRET_KEY')) define('SECRET_KEY',"PRMSUdms_db_key");
+if(!defined('SECRET_IV')) define('SECRET_IV',"PRMSUdms_db_iv");
+?>
