d225bbcf5d733b457efcbb2d8de7e39cfd322129
andi-nieves <andinieves151720@gmail.com>
2023-01-23 21:53:17 +0800

    Update db export data

--- database/prmsudms_db.sql
+++ database/prmsudms_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 09, 2023 at 04:30 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 7.4.33
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 23, 2023 at 01:52 PM
+-- Server version: 10.5.12-MariaDB-cll-lve
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `prmsudms_db`
+-- Database: `u488751301_prmsudorm`
 --
 
 -- --------------------------------------------------------
@@ -37,17 +37,7 @@ CREATE TABLE `account_list` (
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `account_list`
---
-
-INSERT INTO `account_list` (`id`, `code`, `student_id`, `room_id`, `rate`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
-(1, '202205070001', 1, 5, 5000.00, 1, 0, '2022-05-07 13:46:00', '2022-12-02 13:33:39'),
-(6, '', 4, 10, 0.00, 1, 0, '2022-12-12 23:06:12', '2022-12-19 10:19:35'),
-(7, '', 2, 10, 0.00, 1, 0, '2022-12-12 23:35:51', '2022-12-19 10:21:04'),
-(9, '', 3, 6, 0.00, 1, 0, '2022-12-13 00:16:10', '2022-12-13 00:16:10');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -62,7 +52,7 @@ CREATE TABLE `dorm_list` (
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `dorm_list`
@@ -75,9 +65,9 @@ INSERT INTO `dorm_list` (`id`, `name`, `status`, `delete_flag`, `date_created`, 
 (4, 'Female Dorm 2', 1, 0, '2022-12-02 13:25:06', '2022-12-02 13:25:06'),
 (5, 'Male Dorm 101', 0, 0, '2022-12-02 13:26:10', '2022-12-02 13:26:10'),
 (23, 'College of Education Dorm 1', 0, 0, '2022-12-16 16:28:13', '2022-12-16 16:28:22'),
-(25, '233', 1, 0, '2022-12-16 17:31:13', '2022-12-19 08:28:03'),
-(26, '22', 1, 0, '2022-12-16 17:31:16', '2022-12-16 17:40:40'),
-(29, '22355', 1, 0, '2022-12-19 16:42:53', '2022-12-22 18:27:50');
+(25, '233', 1, 1, '2022-12-16 17:31:13', '2023-01-23 13:40:33'),
+(26, '22', 1, 1, '2022-12-16 17:31:16', '2023-01-23 13:40:30'),
+(29, '22355', 1, 1, '2022-12-19 16:42:53', '2023-01-22 18:17:06');
 
 -- --------------------------------------------------------
 
@@ -92,32 +82,7 @@ CREATE TABLE `payment_list` (
   `amount` float(12,2) NOT NULL DEFAULT 0.00,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `payment_list`
---
-
-INSERT INTO `payment_list` (`id`, `account_id`, `month_of`, `amount`, `date_created`, `date_updated`) VALUES
-(0, 3, '2022-12', 450.00, '2022-12-19 16:06:32', '2022-12-19 19:13:11'),
-(3, 1, '2022-04', 5000.00, '2022-12-02 13:35:02', '2022-12-02 13:35:02'),
-(4, 1, '2022-05', 5000.00, '2022-12-02 13:36:45', '2022-12-02 13:36:45'),
-(5, 2, '2022-05', 3500.00, '2022-12-02 13:37:48', '2022-12-02 13:37:48'),
-(6, 3, '2022-12-01', 1000.00, '2022-12-13 00:26:25', '2022-12-13 00:26:25'),
-(8, 3, '2022-08', 201.00, '2022-12-19 15:34:28', '2022-12-19 16:40:21'),
-(9, 0, '2022-03', 2.00, '2022-12-19 15:46:54', '2022-12-19 15:46:54'),
-(10, 0, '2022-03', 2.00, '2022-12-19 15:47:03', '2022-12-19 15:47:03'),
-(11, 0, '2022-03', 2.00, '2022-12-19 15:47:07', '2022-12-19 15:47:07'),
-(12, 0, '2022-10', 200.00, '2022-12-19 15:48:07', '2022-12-19 15:48:07'),
-(13, 0, '2022-10', 200.00, '2022-12-19 15:48:11', '2022-12-19 15:48:11'),
-(14, 0, '2022-10', 200.00, '2022-12-19 15:48:15', '2022-12-19 15:48:15'),
-(15, 3, '2022-11', 233.00, '2022-12-19 15:49:42', '2022-12-19 15:49:42'),
-(21, 3, '2022-08', 1234.00, '2022-12-19 15:56:02', '2022-12-19 16:40:10'),
-(22, 3, '2022-09', 123.00, '2022-12-19 15:57:01', '2022-12-19 15:57:01'),
-(23, 3, '2022-07', 123.00, '2022-12-19 15:57:26', '2022-12-19 15:57:26'),
-(25, 3, '2022-01', 123456.00, '2022-12-19 16:00:25', '2022-12-19 16:00:25'),
-(26, 3, '2022-06', 56433.00, '2022-12-19 16:00:53', '2022-12-19 16:00:53'),
-(28, 2, '2022-12', 200.00, '2022-12-19 19:06:06', '2022-12-19 19:06:06');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -135,7 +100,7 @@ CREATE TABLE `room_list` (
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `room_list`
@@ -144,7 +109,7 @@ CREATE TABLE `room_list` (
 INSERT INTO `room_list` (`id`, `dorm_id`, `name`, `slots`, `price`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
 (1, 1, 'Room 101', 3, 3500.00, 1, 0, '2022-12-02 13:39:43', '2022-12-02 13:39:43'),
 (2, 1, 'Room 102', 4, 3500.00, 1, 0, '2022-12-02 13:58:12', '2022-12-02 13:58:12'),
-(3, 2, 'Room 101', 4, 3500.00, 1, 0, '2022-12-02 14:11:15', '2022-12-02 14:11:15'),
+(3, 2, 'Room 101', 4, 3500.00, 1, 1, '2022-12-02 14:11:15', '2023-01-22 18:17:14'),
 (4, 2, 'Room 102', 4, 3500.00, 1, 0, '2022-12-02 14:15:32', '2022-12-02 14:15:32'),
 (5, 3, 'Room 101', 2, 5000.00, 1, 0, '2022-12-02 14:17:30', '2022-12-02 14:17:30'),
 (6, 4, 'Room 101', 2, 5000.00, 1, 0, '2022-12-02 14:18:45', '2022-12-02 14:18:45'),
@@ -168,6 +133,7 @@ CREATE TABLE `student_list` (
   `firstname` text NOT NULL,
   `middlename` text NOT NULL,
   `lastname` text NOT NULL,
+  `extension` text NOT NULL,
   `department` text NOT NULL,
   `birthdate` date DEFAULT NULL,
   `course` text NOT NULL,
@@ -183,22 +149,9 @@ CREATE TABLE `student_list` (
   `status` tinyint(1) NOT NULL DEFAULT 1,
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `student_list`
---
-
-INSERT INTO `student_list` (`id`, `code`, `firstname`, `middlename`, `lastname`, `department`, `birthdate`, `course`, `gender`, `contact`, `religion`, `email`, `address`, `emergency_name`, `emergency_contact`, `emergency_address`, `emergency_relation`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
-(1, '6231415', 'Juan', 'D', 'dela Cruz', 'College of Engineering', NULL, 'Bachelor of Science in Computer  Science', 'Male', '09123456789', '', 'jdelacruz@gmail.com', '23 St, Here City, Over There Province,  2306', 'Ivan dela Cruz', '09654789123', '23 St, Here City, Over There Province,  2306', 'Father', 1, 0, '2022-12-02 14:22:55', '2022-12-02 14:22:55'),
-(2, 'asd', 'Andy', 'Fuentecilla', 'Nieves', 'asdas', '2022-12-09', 'dasd', 'Male', '09469137286', 'sdasd', 'andinieves151720@gmail.com', 'asdasd', 'Andy Fuentecilla Nieves', 'asd', '', 'asdasd', 0, 0, '2022-12-08 17:41:47', '2022-12-08 17:41:47'),
-(3, '05-11553i', 'Andy', 'Fuentecilla', 'Nieves', 'College of Communication and Information Technology', '1989-12-02', 'Bachelor of Science in Computer Science', 'Male', '09469137286', 'Roman Catholic', 'andinieves151720@gmail.com', 'Purok 2, Bangantalinga', 'Andy Fuentecilla Nieves', '0192039102390123', 'Ohio', 'Twin', 0, 0, '2022-12-08 17:43:53', '2022-12-08 17:43:53'),
-(4, 'xxxx', 'Andy', 'Fuentecilla', 'Nieves', 'x', '2022-12-03', 'x', 'Male', '09469137286', 'xx', 'andinieves151720@gmail.com', 'Purok 2, Bangantalinga', 'Andy Fuentecilla Nieves', 'xx', 'xx', 'xx', 1, 0, '2022-12-08 17:44:52', '2022-12-08 18:08:06'),
-(5, '051234123123', 'Codi Kyler', 'Cayaban', 'Nieves', 'Department of Accountancy', '1989-12-23', 'BS in Accountancy Technology', 'Male', '09297752410', 'Roman Catholic', 'codi@nieves.com', 'Purok 2, Bangantalinga', 'Andy Fuentecilla Nieves', '09297752410', 'Purok 2, Bangantalinga', 'Father', 1, 0, '2022-12-23 10:37:02', '2022-12-23 10:37:02'),
-(6, '22-123456a', 'Andy', 'Fuentecilla', 'Nieves', 'Department of Education', '2022-12-02', 'Bachelor of Elementary Education', 'Male', '09469137286', 'RC', 'codi@nieves.com', 'Purok 2, Bangantalinga', 'Andy Fuentecilla Nieves', '231231231212312312313123123', '123', 'asdasd', 1, 0, '2022-12-23 10:39:46', '2022-12-23 10:39:46'),
-(7, 'xxxxx', 'Andy', 'Fuentecilla', 'Nieves', 'Department of Education', '2022-12-03', 'Bachelor of Elementary Education', 'Male', '09469137286', 'asd', 'andinieves@gmail.com', 'Purok 2, Bangantalinga', 'Andy Fuentecilla Nieves', '09469137286', '123', 'asdasd', 1, 0, '2022-12-23 12:25:35', '2022-12-23 12:25:35'),
-(8, '22-123456at', 'Andy', 'Fuentecilla', 'Nieves', 'Department of Education', '2022-12-02', 'Bachelor of Elementary Education', 'Male', '09469137286', '123', 'zbsd@gmail.com', 'Purok 2, Bangantalinga', 'Andy Fuentecilla Nieves', '123', 'Purok 2, Bangantalinga', '23', 1, 0, '2022-12-23 12:26:20', '2022-12-23 12:26:20');
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `approved` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -210,22 +163,20 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `type` int(11) NOT NULL DEFAULT 3,
+  `type` int(11) NOT NULL DEFAULT 2,
   `status` int(1) NOT NULL DEFAULT 1,
   `delete_flag` int(1) NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `type`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
-(1, 'Admin', 'Admin123', 1, 1, 0, '2022-12-21 08:35:22', '2022-12-21 08:35:22'),
-(2, 'Dhey', '123456', 1, 1, 0, '2022-12-21 08:35:22', '2022-12-21 08:35:22'),
-(26, 'andy', '123456', 1, 1, 0, '2022-12-22 18:39:32', '2022-12-22 18:39:32'),
-(27, 'zbsd@gmail.com', '', 3, 1, 0, '2022-12-23 12:26:20', '2022-12-23 12:26:20');
+(1, 'Admin', 'P@ssw0rd123!', 1, 1, 0, '2022-12-21 08:35:22', '2022-12-21 08:35:22'),
+(2, 'Dhey', 'P@ssw0rd123!', 2, 1, 0, '2022-12-21 08:35:22', '2022-12-21 08:35:22');
 
 -- --------------------------------------------------------
 
@@ -238,7 +189,7 @@ CREATE TABLE `user_meta` (
   `user_id` int(11) NOT NULL,
   `meta_key` text NOT NULL,
   `meta_value` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user_meta`
@@ -248,13 +199,7 @@ INSERT INTO `user_meta` (`id`, `user_id`, `meta_key`, `meta_value`) VALUES
 (25, 1, 'first_name', 'Admin'),
 (26, 2, 'first_name', 'Dhey'),
 (28, 1, 'last_name', 'Dhey'),
-(30, 2, 'last_name', 'Dhey'),
-(77, 26, 'first_name', 'Andyx'),
-(78, 26, 'middle_name', 'Fuentecillax'),
-(79, 26, 'last_name', 'Nievesx'),
-(83, 26, 'profile_avatar', 'profile-avatar-26.png'),
-(84, 2, 'middle_name', 'Yehd'),
-(85, 2, 'profile_avatar', 'profile-avatar-2.png');
+(30, 2, 'last_name', 'Dhey');
 
 --
 -- Indexes for dumped tables
@@ -334,19 +279,19 @@ ALTER TABLE `room_list`
 -- AUTO_INCREMENT for table `student_list`
 --
 ALTER TABLE `student_list`
-  MODIFY `id` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `user_meta`
 --
 ALTER TABLE `user_meta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
