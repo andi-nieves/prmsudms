51e2b83e950ddf703f117787f879a8afd75d490a
andi-nieves <andinieves151720@gmail.com>
2023-02-08 20:55:19 +0800

    Fix print header

--- admin/print_monthly_collection.php
+++ admin/print_monthly_collection.php
@@ -112,7 +112,12 @@ $name = $dbhelper->get_user_meta($_SESSION['id'], 'first_name'). ' '.$dbhelper->
 
 <body>
     <div class="wrapper">
-        <p class="text-center">Republic of the Philippines</br /><b>Dorm collection office</b></p>
+        <p class="text-center">
+        Republic of the Philippines<br />
+        <b>PRESIDENT RAMON MAGSAYSAY STATE UNIVERSITY</b><br />
+        Dorm Collection Office<br />
+        Iba, Zambales
+        </p>
         <p>Month of: <?= $month ?>
         </p>
         <table class="table">
